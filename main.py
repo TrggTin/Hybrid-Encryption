12a5b2e8b28fbde2d4e022406b140fe38663c873
Rio <100864189+TrggTin@users.noreply.github.com>
2024-11-07 13:48:02 +0700

    test1

---
+++ main.py
@@ -0,0 +1,7 @@
+#File set up
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Util.Padding import pad, unpad
+import hashlib
+from base64 import b64encode, b64decode
